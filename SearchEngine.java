3803dcb102f0573d24aecebd285eed797376b318
jadontran <114566278+jadontran@users.noreply.github.com>
2022-10-14 22:50:16 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
 
@@ -22,6 +23,17 @@ public class SearchEngine {
                 }
             }
             return arr;
-        }
     }
+
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
 
